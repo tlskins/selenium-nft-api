0423dc5b1d9e71646522e6f509c12c0928fcea74
Tim Lee <tlee87@gmail.com>
2022-01-30 11:57:59 -0500

    fixes, add retries, tweak timing

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ async function scrapeCollections(){
   if (!collMaps) throw Error("Collections not found")
   
   var options   = new chrome.Options()
-  // options.addArguments('headless') // note: without dashes
   // options.addArguments('disable-gpu')
+  options.addArguments('headless') // note: without dashes
   options.addArguments('--window-size=1920,1080')
   options.addArguments('--disable-blink-features=AutomationControlled')
   options.addArguments('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36')
@@ -28,186 +28,194 @@ async function scrapeCollections(){
     .setChromeOptions(options)
     .build()
 
-  for (let i=0;i<collMaps.length;i++) {
-    const collMap = collMaps[i]
-    await scrape(db, driver, collMap)
-    sleep(500)
+  console.log(`${moment().format()}: scraping ${collMaps.length} collections`)
+  while ( true ) {
+    // *** TESTING ***
+    // const collMap = collMaps.find( c => c.coll === "Solsteads" )
+    // if ( !collMap ) throw "Collection mapping not found"
+    // await scrape(db, driver, collMap)
+
+    for (let i=0;i<collMaps.length;i++) {
+      let fails = 0
+      const collMap = collMaps[i]
+      try {
+        await scrape(db, driver, collMap)
+      } catch(error) {
+        const errMsg = `error scraping ${collMap.coll} ` + error
+        console.log(errMsg)
+
+        fails++
+        if ( fails <= 2 ) i--
+
+        await upsertListings(db, {
+          _id: collMap.coll,
+          updAt: moment().format(),
+          errMsg,
+        })
+      }
+      sleep(100)
+    }
   }
 
-  await driver.quit()
 }
 
 async function scrape(db, driver, collMap) {
-  console.log(`scraping ${collMap.coll}...`)
+  console.log(`${moment().format()}: scraping ${collMap.coll}...`)
   const { coll, edenColl } = collMap
   const now = moment().format()
 
-  try {
-    const url = `${edenUrl}/${edenColl}`
-    await driver.get(url)
+  const url = `${edenUrl}/${edenColl}`
+  await driver.get(url)
 
-    await sleep(1500) // collection-filter doesnt render immediately
+  await sleep(300) // collection-filter doesnt render immediately
 
-    // get number listed
-    const attrsRow = await driver.findElement(By.className("attributes-row"))
-    const collAttrs = await attrsRow.findElements(By.className("attributes-column"))
-    const numListTxt = await collAttrs[collAttrs.length-1].findElement(By.className("attribute-value")).getText()
-    const listedCount = parseInt( numListTxt )
+  // get number listed
+  const attrsRow = await driver.findElement(By.className("attributes-row"))
+  const collAttrs = await attrsRow.findElements(By.className("attributes-column"))
+  const numListTxt = await collAttrs[collAttrs.length-1].findElement(By.className("attribute-value")).getText()
+  const listedCount = parseInt( numListTxt )
 
-    // find sale and items buttons
-    let viewBtns = await driver.findElements(By.className("me-tab2"))
-    const salesBtn = viewBtns[1]
+  // find sale and items buttons
+  let viewBtns = await driver.findElements(By.className("me-tab2"))
+  const salesBtn = viewBtns[1]
 
-    // get recent activity
-    console.log("clicking sales...")
-    await salesBtn.click()
-    await sleep(500)
+  // get recent activity
+  console.log("clicking sales...")
+  await salesBtn.click()
+  // await driver.executeScript("arguments[0].click()", salesBtn)
+  await sleep(150)
 
-    const sales = []
-    for (let currSalesPg=0;currSalesPg<5;currSalesPg++) {
-      // scrape sales
-      console.log(`scraping sales page ${currSalesPg}`)
-      const salesTable = await driver.findElement(By.className("me-table__container"))
-      const salesRows = await salesTable.findElements(By.css("tr"))
-      console.log(`${salesRows.length} sales found`)
-      for(let i=0;i<salesRows.length;i++) {
-        const salesRow = salesRows[i]
-        const cells = await salesRow.findElements(By.css("td"))
-        if ( cells.length === 0 ) continue
+  const sales = []
+  for (let currSalesPg=0;currSalesPg<2;currSalesPg++) {
+    // scrape sales
+    console.log(`scraping sales page ${currSalesPg}`)
+    const salesTable = await driver.findElement(By.className("me-table__container"))
+    const salesRows = await salesTable.findElements(By.css("tr"))
+    console.log(`${salesRows.length} sales found`)
+    for(let i=0;i<salesRows.length;i++) {
+      const salesRow = salesRows[i]
+      const cells = await salesRow.findElements(By.css("td"))
+      if ( cells.length === 0 ) continue
 
-        const title = await cells[1].getText()
-        const tokenNum = parseInt(title.match(/#(\d+)/)[1])
-        const priceStr = await cells[5].getText()
-        const price = parseFloat(priceStr.replace(/ SOL/, ""))
-        const url = await salesRow.findElement(By.linkText(title)).getAttribute("href")
-        const tokenAddr = url.match(/item-details\/([a-zA-Z0-9]+)/)[1]
-        // const imgTxt = await salesRow.findElement(By.className("card-img-top")).getAttribute("src")
-        // const img = imgTxt.match(/(https:\/\/metadata.+)/)[1]
-        const txUrl = await cells[2].findElement(By.css("a")).getAttribute("href")
-        const txAddr = txUrl.match(/\/tx\/([a-zA-Z0-9]+)/)[1]
-        const agoStr = await cells[4].getText()
-        const date = getDateFrom(agoStr).format()
-
-        sales.push({
-          _id: `Magic Eden-${txAddr}`,
-          nm: title,
-          tokenNum,
-          price,
-          date,
-          mp: "Magic Eden",
-          coll,
-          tokenAddr,
-        })
-      }
-
-      // next page
-      if ( currSalesPg < salesRows.length-1 ) {
-        const tableNav = await driver.findElement(By.className("me-table__pagination"))
-        const tableNavBtns = await tableNav.findElements(By.className("me-table__pagination-btn"))
-        const nextPgBtn = tableNavBtns[2]
-
-        // reached end of pages
-        const disableTxt = await nextPgBtn.getAttribute("disabled")
-        if ( disableTxt ) break
-
-        await driver.executeScript("window.scrollTo(0, document.body.scrollHeight)")
-        console.log(`clicking next page from ${currSalesPg}`)
-        await driver.executeScript("arguments[0].click()", nextPgBtn)
-        await sleep(1000)
-      }
-    }
-
-    // get listings
-    console.log("clicking listings...")
-    await driver.executeScript("window.scrollTo(0,0)")
-    const page = await driver.findElement(By.css("body"))
-    await page.sendKeys(Key.CONTROL + Key.HOME)
-    viewBtns = await driver.findElements(By.className("me-tab2"))
-    const itemsBtn = viewBtns[0]
-    console.log('clickin items btn', await itemsBtn.getText())
-    await driver.executeScript("arguments[0].click()", itemsBtn)
-    // await itemsBtn.click()
-    await sleep(1000)
-    
-    // sort listings cheapest first
-    console.log("sorting listings...")
-    const filtersEl = await driver.findElement(By.className("collection-filter"))
-    const sortSel = await filtersEl.findElement(By.className("me-dropdown-container"))
-    await driver.executeScript("arguments[0].scrollIntoView();", sortSel)
-    await sortSel.click()
-    const sortOpts = await filtersEl.findElements(By.className("me-select-item"))
-    for(let i=0;i<sortOpts.length;i++) {
-      const opt = sortOpts[i]
-      const optTxt = await opt.getText()
-      if ( optTxt.toLocaleLowerCase().includes("low to high")) {
-        await opt.click()
-        break
-      }
-    }
-  
-    // scroll down to load more listings
-    console.log("scrolling for listings...")
-    await sleep(500);
-    driver.executeScript("window.scrollTo(0, document.body.scrollHeight)")
-    await sleep(500)
-    driver.executeScript("window.scrollTo(0, document.body.scrollHeight)")
-    await sleep(500)
-    driver.executeScript("window.scrollTo(0, document.body.scrollHeight)")
-    await sleep(500)
-    driver.executeScript("window.scrollTo(0, document.body.scrollHeight)")
-    await sleep(500)
-
-    // scrape listings
-    const els = await driver.findElements(By.className("grid-card__main"))
-    const listings = []
-    let floor = 0.0
-    for(let i=0;i<els.length;i++) {
-      const el = els[i]
-      const title = await el.findElement(By.className("grid-card__title")).getText()
-      const tokenNum = parseInt(title.match(/#(\d+)/)[1])
-      const priceStr = await el.findElement(By.className("card__price")).getText()
+      const title = await cells[1].getText()
+      const tokenNum = getTokenNum(title)
+      const priceStr = await cells[5].getText()
       const price = parseFloat(priceStr.replace(/ SOL/, ""))
-      const url = await el.findElement(By.linkText(title)).getAttribute("href")
+      const url = await salesRow.findElement(By.linkText(title)).getAttribute("href")
       const tokenAddr = url.match(/item-details\/([a-zA-Z0-9]+)/)[1]
-      const img = await el.findElement(By.className("card-img-top")).getAttribute("src")
+      // const imgTxt = await salesRow.findElement(By.className("card-img-top")).getAttribute("src")
+      // const img = imgTxt.match(/(https:\/\/metadata.+)/)[1]
+      const txUrl = await cells[2].findElement(By.css("a")).getAttribute("href")
+      const txAddr = txUrl.match(/\/tx\/([a-zA-Z0-9]+)/)[1]
+      const agoStr = await cells[4].getText()
+      const date = getDateFrom(agoStr).format()
 
-      if ( floor === 0.0) floor = price
-
-      listings.push({
-        _id: `${"Magic Eden"}-${tokenAddr}`,
-        updAt: now,
-        title,
-        img,
-        url,
-        tokenAddr,
+      sales.push({
+        _id: `Magic Eden-${txAddr}`,
+        nm: title,
         tokenNum,
-        coll,
-        mp: "Magic Eden",
         price,
-        forSale: true,
+        date,
+        mp: "Magic Eden",
+        coll,
+        tokenAddr,
       })
     }
-    
-    console.log('upserting listings...')
-    await upsertListings(db, {
-      _id: coll,
+
+    // next page
+    if ( currSalesPg < salesRows.length-1 ) {
+      const tableNav = await driver.findElement(By.className("me-table__pagination"))
+      const tableNavBtns = await tableNav.findElements(By.className("me-table__pagination-btn"))
+      const nextPgBtn = tableNavBtns[2]
+
+      // reached end of pages
+      const disableTxt = await nextPgBtn.getAttribute("disabled")
+      if ( disableTxt ) break
+
+      await driver.executeScript("window.scrollTo(0, document.body.scrollHeight)")
+      console.log(`clicking next page from ${currSalesPg}`)
+      await driver.executeScript("arguments[0].click()", nextPgBtn)
+      await sleep(50)
+    }
+  }
+
+  // get listings
+  console.log("clicking listings...")
+  await driver.executeScript("window.scrollTo(0,0)")
+  const page = await driver.findElement(By.css("body"))
+  await page.sendKeys(Key.CONTROL + Key.HOME)
+  viewBtns = await driver.findElements(By.className("me-tab2"))
+  const itemsBtn = viewBtns[0]
+  console.log('clickin items btn', await itemsBtn.getText())
+  await driver.executeScript("arguments[0].click()", itemsBtn)
+  await sleep(50)
+  
+  // sort listings cheapest first
+  console.log("sorting listings...")
+  const filtersEl = await driver.findElement(By.className("collection-filter"))
+  const sortSel = await filtersEl.findElement(By.className("me-dropdown-container"))
+  await driver.executeScript("arguments[0].scrollIntoView();", sortSel)
+  await sortSel.click()
+  const sortOpts = await filtersEl.findElements(By.className("me-select-item"))
+  for(let i=0;i<sortOpts.length;i++) {
+    const opt = sortOpts[i]
+    const optTxt = await opt.getText()
+    if ( optTxt.toLocaleLowerCase().includes("low to high")) {
+      await opt.click()
+      break
+    }
+  }
+
+  // scroll down to load more listings
+  console.log("scrolling for listings...")
+  await sleep(20);
+  driver.executeScript("window.scrollTo(0, document.body.scrollHeight)")
+  await sleep(20)
+  driver.executeScript("window.scrollTo(0, document.body.scrollHeight)")
+  await sleep(150)
+
+  // scrape listings
+  const els = await driver.findElements(By.className("grid-card__main"))
+  const listings = []
+  let floor = 0.0
+  for(let i=0;i<els.length;i++) {
+    const el = els[i]
+    const title = await el.findElement(By.className("grid-card__title")).getText()
+    const tokenNum = getTokenNum(title)
+    const priceStr = await el.findElement(By.className("card__price")).getText()
+    const price = parseFloat(priceStr.replace(/ SOL/, ""))
+    const url = await el.findElement(By.linkText(title)).getAttribute("href")
+    const tokenAddr = url.match(/item-details\/([a-zA-Z0-9]+)/)[1]
+    const img = await el.findElement(By.className("card-img-top")).getAttribute("src")
+
+    if ( floor === 0.0) floor = price
+
+    listings.push({
+      _id: `${"Magic Eden"}-${tokenAddr}`,
       updAt: now,
-      floor,
-      listedCount,
-      listings,
-      sales,
-      errMsg: "",
-    })
-    console.log(`upserted ${listings.length} listings for ${coll}...`)
-  } catch(error) {
-    const errMsg = `error scraping ${coll} ` + error
-    console.log(errMsg)
-    await upsertListings(db, {
-      _id: coll,
-      updAt: now,
-      errMsg,
+      title,
+      img,
+      url,
+      tokenAddr,
+      tokenNum,
+      coll,
+      mp: "Magic Eden",
+      price,
+      forSale: true,
     })
   }
+  
+  console.log('upserting listings...')
+  await upsertListings(db, {
+    _id: coll,
+    updAt: now,
+    floor,
+    listedCount,
+    listings,
+    sales,
+    errMsg: "",
+  })
+  console.log(`${moment().format()}: upserted ${listings.length} listings for ${coll}...`)
 }
 
 // database
@@ -248,6 +256,14 @@ function getDateFrom(agoStr) {
   const uom = agoStr.match(/\d+ ([a-z]+) ago/)[1]
 
   return moment().add(units, uom)
+}
+
+function getTokenNum(title) {
+  const matches = title.match(/#(\d+)/)
+  if ( !matches || matches.length < 2 ) {
+    return 0
+  }
+  return parseInt(matches[1])
 }
 
 const edenUrl = "https://magiceden.io/marketplace"
